46f67fa2bcb36f7d005fbcb9c878b102ebb916f3
mc-soft <matthew.croston@btinternet.com>
2018-11-09 10:02:18 +0000

    Initial commit

---
+++ woocommerce-gift-wrap.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WooCommerce Gift Wrap
+Plugin URI: https://github.com/mntcode/woocommerce-gift-wrap
+Description: Add a customisation option to your products, this can be free or charged.
+Version: 1.0
+Author: Matthew Croston
+Author URI: https://mtcode.co.uk/
+Requires at least: 3.5
+Tested up to: 4.9
+*/
+
+class WC_Gift_Wrap {
+
+}
+
+new WC_Gift_Wrap();
