1bb29cbc766987c3aac1cc9b51bb02eab9f257d8
mc-soft <matthew.croston@btinternet.com>
2018-11-12 15:35:54 +0000

    Added giftwrap panel to woocommerce products

--- includes/wc-giftwrap.class.php
+++ includes/wc-giftwrap.class.php
@@ -8,4 +8,19 @@
 
 class WC_GiftWrap {
 
+    public function __construct() {
+        // Display on backend product admin pages.
+        add_filter('woocommerce_product_data_tabs', array($this, 'add_giftwrap_product_panel'));
+    }
+
+    public function add_giftwrap_product_panel($tabs) {
+        $tabs['giftwrap'] = array(
+            'label'     => __('Gift Wrap', 'woocommerce'),
+            'target'    => 'giftwrap_options',
+            'class'     => array('show_if_simple', 'show_if_variable')
+        );
+
+        return $tabs;
+    }
+
 }
