204e871f992d17df6af01dfcf322193c0046d088
mc-soft <matthew.croston@btinternet.com>
2018-11-13 10:16:50 +0000

    Added product page HTML template

--- includes/wc-giftwrap.class.php
+++ includes/wc-giftwrap.class.php
@@ -18,6 +18,9 @@ class WC_GiftWrap {
         // Save the product meta options.
         add_action('woocommerce_process_product_meta_simple', array($this, 'save_giftwrap_fields'));
         add_action('woocommerce_process_product_meta_variable', array($this, 'save_giftwrap_fields'));
+
+        // Display checkbox on product pages.
+        add_action('woocommerce_after_add_to_cart_button', array($this, 'product_page_html'));
     }
 
     public function add_giftwrap_product_tab($tabs) {
@@ -46,6 +49,25 @@ class WC_GiftWrap {
 
         if (isset($_POST['_giftwrapping_message'])) {
             update_post_meta($post_id, '_giftwrapping_message', wc_clean($_POST['_giftwrapping_message']));
+        }
+    }
+
+    public function product_page_html() {
+        $is_wrappable = get_post_meta(get_the_ID(), '_giftwrapping_enabled', true) == 'yes';
+
+        if ($is_wrappable) {
+            $is_checked = !empty($_REQUEST['gift_wrap_item']); // Allows choice to persist back/forward navigation.
+            $giftwrapping_cost = get_post_meta(get_the_ID(), '_giftwrapping_cost', true);
+            $giftwrapping_message = get_post_meta(get_the_ID(), '_giftwrapping_message', true);
+
+            if ($giftwrapping_cost == '') { $giftwrapping_cost = 0; }
+            if ($giftwrapping_message == '') { $giftwrapping_message = $this->default_giftwrap_message; }
+
+            wc_get_template('product_page_checkbox.php', array(
+                'is_checked'                => $is_checked,
+                'giftwrapping_cost'         => $giftwrapping_cost,
+                'giftwrapping_message'      => $giftwrapping_message
+            ), 'woocommerce-gift-wrap', WC_GIFTWRAP_PATH . '/templates/');
         }
     }
 }
