e690e24e4e06772ea88b16c53d170e9a478784f0
mc-soft <matthew.croston@btinternet.com>
2018-11-13 13:58:46 +0000

    Added logic to add gift wrapping to an order

--- includes/wc-giftwrap.class.php
+++ includes/wc-giftwrap.class.php
@@ -8,6 +8,7 @@
 
 class WC_GiftWrap {
 
+    private $default_giftwrap_cost = 0;
     private $default_giftwrap_message = 'Gift wrap this product';
 
     public function __construct() {
@@ -21,6 +22,13 @@ class WC_GiftWrap {
 
         // Display checkbox on product pages.
         add_action('woocommerce_after_add_to_cart_button', array($this, 'product_page_html'));
+
+        // Add meta to order.
+        add_filter('woocommerce_add_cart_item_data', array($this, 'add_cart_item_data'), 10, 2);
+        add_filter('woocommerce_add_cart_item', array($this, 'add_cart_item'), 10, 1);
+        add_filter('woocommerce_get_item_data', array($this, 'get_item_data'), 10, 2);
+        add_filter('woocommerce_get_cart_item_from_session', array($this, 'get_cart_item_from_session'), 10, 2);
+        add_action('woocommerce_add_order_item_meta', array($this, 'add_order_item_meta'), 10, 2);
     }
 
     public function add_giftwrap_product_tab($tabs) {
@@ -60,7 +68,7 @@ class WC_GiftWrap {
             $giftwrapping_cost = get_post_meta(get_the_ID(), '_giftwrapping_cost', true);
             $giftwrapping_message = get_post_meta(get_the_ID(), '_giftwrapping_message', true);
 
-            if ($giftwrapping_cost == '') { $giftwrapping_cost = 0; }
+            if ($giftwrapping_cost == '') { $giftwrapping_cost = $this->default_giftwrap_cost; }
             if ($giftwrapping_message == '') { $giftwrapping_message = $this->default_giftwrap_message; }
 
             wc_get_template('product_page_checkbox.php', array(
@@ -68,6 +76,65 @@ class WC_GiftWrap {
                 'giftwrapping_cost'         => $giftwrapping_cost,
                 'giftwrapping_message'      => $giftwrapping_message
             ), 'woocommerce-gift-wrap', WC_GIFTWRAP_PATH . '/templates/');
+        }
+    }
+
+    public function add_cart_item_data($cart_item_data, $product_id) {
+        $is_wrappable = get_post_meta($product_id, '_giftwrapping_enabled', true) == 'yes';
+
+        if (!empty($_POST['gift_wrap_item']) && $is_wrappable) {
+            $cart_item_data['gift_wrap_item'] = true;
+        }
+
+        return $cart_item_data;
+    }
+
+    public function add_cart_item($cart_item_data) {
+        if (!empty($cart_item_data['gift_wrap_item'])) {
+            $current_price = $cart_item_data['data']->get_price();
+            $giftwrapping_cost = get_post_meta($cart_item_data['product_id'], '_giftwrapping_cost', true);
+
+            if ($giftwrapping_cost == '') {
+                $giftwrapping_cost = $this->default_giftwrap_cost;
+            }
+
+            $cart_item_data['data']->set_price($current_price + $giftwrapping_cost);
+        }
+
+        return $cart_item_data;
+    }
+
+    public function get_item_data($item_data, $cart_item) {
+        if (!empty($cart_item['gift_wrap_item'])) {
+            $item_data[] = array(
+                'name'      => __('Gift Wrapped', 'woocommerce'),
+                'value'     => __('Yes', 'woocommerce'),
+                'display'   => __('Yes', 'woocommerce')
+            );
+        }
+
+        return $item_data;
+    }
+
+    public function get_cart_item_from_session($cart_item, $values) {
+        if (!empty($values['gift_wrap_item'])) {
+            $cart_item['gift_wrap_item'] = true;
+            $current_price = $cart_item['data']->get_price();
+            $giftwrapping_cost = get_post_meta($cart_item['data']->id, '_giftwrapping_cost', true);
+
+            if ($giftwrapping_cost == '') {
+                $giftwrapping_cost = $this->default_giftwrap_cost;
+            }
+
+            $cart_item['data']->set_price($current_price + $giftwrapping_cost);
+        }
+
+        return $cart_item;
+    }
+
+    public function add_order_item_meta($item_id, $cart_item) {
+        if (!empty($cart_item['gift_wrap_item'])) {
+            wc_add_order_item_meta($item_id, __('Gift Wrapped', 'woocommerce'), __('Yes', 'woocommerce') );
         }
     }
 }
